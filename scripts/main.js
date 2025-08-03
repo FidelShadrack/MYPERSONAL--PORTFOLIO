02721b40a62647cc3bc9b8a43e4ac430e9862dba
mongarejoshua <mogakahjoshua254@gmail.com>
2025-08-03 19:32:46 +0300

    4th commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+
+    function toggleMenu() {
+        const humburger = document.querySelector('.humburger');
+        const navList = document.querySelector('.navbar ul');
+        humburger.classList.toggle('active');
+        navList.classList.toggle('active');
+    }

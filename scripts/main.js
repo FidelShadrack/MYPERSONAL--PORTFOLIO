eb6bf6b9827a3478486e76e58cc56c5d28f4ee39
mongarejoshua <mogakahjoshua254@gmail.com>
2025-08-04 00:30:41 +0300

    5th commit

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-
-    function toggleMenu() {
-        const humburger = document.querySelector('.humburger');
-        const navList = document.querySelector('.navbar ul');
-        humburger.classList.toggle('active');
-        navList.classList.toggle('active');
-    }
